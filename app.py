de3edd127b3f96458f0f07f7c4a74bdbda02a399
Anonni Mousse <61146153+maridasi47500@users.noreply.github.com>
2026-03-12 22:32:50 -0300

    lyg

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from yourappdb import query_db, get_db
+from flask import g
+
+app = Flask(__name__)
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+init_db()
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route("/")
+def hello_world():
+    user = query_db('select * from hits')
+    the_username = "anonyme"
+    one_user = query_db('select * from contacts where first_name = ?',
+                [the_username], one=True)
+    return render_template("hey.html", users=user, one_user=one_user)
